32bfffd5e4bac6806540cb78bc1541e9adc45c22
MeghaSJagdale <megha.jagdale51@gmail.com>
2018-10-30 14:54:39 +0530

    all commit

---
+++ 2D Array/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2D_Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] a = new int[3, 3] {{7,1,8},{1,2,6},{4,5,2} };
+            twoarray c = new twoarray();
+            c.print(a);
+            c.add(a);
+            Console.Read();
+        }
+    }
+}
---
+++ 2D Array/twoarray.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2D_Array
+{
+    class twoarray
+    {
+        public int x;
+ 
+        public void print(int[,] a)
+        {
+            for(int i=0;i<a.GetLength(0);i++)
+            {
+                for (int j = 0; j <a.GetLength(1); j++)
+                {
+                    Console.Write(" " + a[i,j]);
+                }
+                Console.WriteLine("");
+            }
+        }
+
+        public void add(int[,] a)
+        {
+            for (int i = 0; i < a.GetLength(0); i++)
+            {
+                for (int j = 0; j < a.GetLength(1); j++)
+                {
+                    x = x + a[i, j];
+                 }
+            }
+            Console.WriteLine("sum: " + x);
+        }
+    }
+}
